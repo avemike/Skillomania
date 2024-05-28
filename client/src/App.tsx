1e62376579d2abbf077157307ec4972a6c4e3f94
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-05-28 22:35:45 +0200

    add fixed problem no work done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>skibidi</div>
+      <div className="w-96 h-96 bg-gradient-to-r from-indigo-300 via-purple-400 to-pink-300 rounded-lg shadow-lg shadow-neutral-400"></div>
     </>
   );
 }
