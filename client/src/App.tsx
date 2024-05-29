4b1bcb0f3658d4acd76189fff3a3592b363bcda4
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-05-29 15:51:26 +0200

    add front page example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,22 @@ function App() {
 
   return (
     <>
-      <div className="w-96 h-96 bg-gradient-to-r from-indigo-300 via-purple-400 to-pink-300 rounded-lg shadow-lg shadow-neutral-400"></div>
+      <div className="absolute left-4 top-4 w-16 h-16 bg-gradient-to-r from-pink-500 to-violet-500 rounded-lg flex items-center justify-center text-white font-bold  text-2xl">
+        S
+      </div>
+      <div className="flex flex-col items-start absolute left-36 top-1/3">
+        <h1 className="font-bold text-7xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+          WELCOME
+        </h1>
+        <h2 className="text-2xl mx-3 mb-2">
+          TO THE <b>SKILLOMANIA</b>
+        </h2>
+        <p className="mx-3">On this webside we upgrade our skills.</p>
+        <p className="mx-3 mb-4">If you would be intrested.</p>
+        <button className="mx-3 hover:shadow-indigo-300/50 shadow-lg shadow-indigo-400/50 text-white bg-gradient-to-r from-pink-500 to-violet-500">
+          Check us out!
+        </button>
+      </div>
     </>
   );
 }
