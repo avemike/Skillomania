9858dbdb76df6a6af0f603466faf8fb92872eaf3
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-05-30 12:57:34 +0200

    add error page, about page and segregated everything

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,44 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StandardLayout } from "./layouts/StandardLayout";
+import { WelcomePage } from "./pages/WelcomePage";
+import { About } from "./pages/About";
+import { Error } from "./pages/Error";
+/**
+ * @param {Object} props
 
+ * @return {JSX.Element}
+ */
+function StandardPage({ page }) {
+  return <StandardLayout>{page}</StandardLayout>;
+}
+
+StandardPage.propTypes = {
+  page: PropTypes.element.isRequired,
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StandardPage page={<WelcomePage />} />,
+  },
+  {
+    path: "/About",
+    element: <StandardPage page={<About />} />,
+  },
+  {
+    path: "/*",
+    element: <StandardPage page={<Error />} />,
+  },
+]);
+
+/**
+ * Renders the Main App component - bootstrap for the entire app.
+ * @return {JSX.Element} The rendered App component.
+ */
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      {" "}
-      <div className="bg-[url('./assets/welcome_website.svg')] bg-center bg-cover h-dvh w-svw absolute top-0 left-0">
-        <div className="absolute right-28 top-10 flex gap-20">
-          <h3 className="font-bold">Home</h3>
-          <h3 className="font-bold">About</h3>
-          <h3 className="font-bold">Contacts</h3>
-        </div>
-        <div className="flex flex-col items-start absolute left-60 top-1/3">
-          <h1 className="font-bold text-7xl bg-clip-text text-black">
-            Welcome
-          </h1>
-          <h2 className="text-2xl my-4 text-gray-500">
-            To the <b>skillomania</b>
-          </h2>
-          <p className="text-gray-500">
-            On this webside we upgrade our skills.
-          </p>
-          <p className="text-gray-500 mb-6">If you would be intrested.</p>
-          <button className=" hover:shadow-indigo-100/50 shadow-lg shadow-indigo-100/50 text-white bg-black">
-            Check us out!
-          </button>
-        </div>
-        <div className="absolute flex bottom-16 left-20 border-t-gray-100 border-t-2 w-11/12">
-          <p className="text-gray-400 pt-14 ml-12  ">Skillomania</p>
-        </div>
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
