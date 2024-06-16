85306ad8bc911c76a449ff23b12d5a12936839e0
michal.gajek <michal.gajek@naturaily.com>
2024-06-16 21:59:31 +0200

    chore(db): add db purge script

---
+++ server/src/database/purge.ts
@@ -0,0 +1,35 @@
+import "dotenv/config";
+
+import readlineSync from "readline-sync";
+import { initializeDatabase } from "./initializeDatabase";
+
+(async () => {
+  const areYouSure = readlineSync.question(
+    "Are you sure you want to purge the database? (yes/no) "
+  );
+  if (areYouSure.toLowerCase() !== "yes") {
+    console.log("Aborted.");
+    process.exit(0);
+  }
+
+  const whatsTheSquareRootOfNine = readlineSync.question(
+    "What's the square root of nine? "
+  );
+
+  if (whatsTheSquareRootOfNine !== "3") {
+    console.log("Incorrect.");
+    process.exit(0);
+  }
+
+  console.log("Purging the database...");
+  const connection = await initializeDatabase();
+
+  await connection.query("TRUNCATE TABLE user_challenge CASCADE");
+  await connection.query("TRUNCATE TABLE app_user CASCADE");
+  await connection.query("TRUNCATE TABLE challenge_challenge_series CASCADE");
+  await connection.query("TRUNCATE TABLE challenge CASCADE");
+  await connection.query("TRUNCATE TABLE challenge_series CASCADE");
+  await connection.query("TRUNCATE TABLE seeds CASCADE");
+
+  console.log("Database purged.");
+})();
