2e6b6332e95c83e8d295938a6c01b5857dc3bb4a
michal.gajek <michal.gajek@naturaily.com>
2024-06-16 17:25:23 +0200

    feat(db): create user, user_challenge and user_challenge_series tables

---
+++ server/src/database/migrations/1718551389768-create-user-table.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUserTable1718551389768 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE user (
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(100) NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                deleted_at TIMESTAMP WITH TIME ZONE
+            );
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        DROP TABLE user;
+    `);
+  }
+}
