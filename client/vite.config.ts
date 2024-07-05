443cacc5c11e4fdc463992e06eb7aae02a9daa90
michal.gajek <michal.gajek@naturaily.com>
2024-07-05 23:55:28 +0200

    chore: introduce vite env validator

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { Schema, ValidateEnv } from "@julr/vite-plugin-validate-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ValidateEnv({
+      VITE_SERVER_BASE_URL: Schema.string(),
+      VITE_OAUTH_CLIENT_ID: Schema.string(),
+    }),
+  ],
   mode: "development",
 });
