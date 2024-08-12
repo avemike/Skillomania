e5c379e136b971f6638df01144515f0594812b87
michal.gajek <michal.gajek@naturaily.com>
2024-08-12 19:56:05 +0200

    feat: add logout option to Avatar's menu

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -1,8 +1,9 @@
 import { useAuth } from "../AuthenticationProvider";
+import { Menu } from "./Menu";
 
 /** Round div with initials */
 export function Avatar() {
-  const { authUser } = useAuth();
+  const { authUser, handleLogout } = useAuth();
 
   if (!authUser) {
     return null;
@@ -10,9 +11,18 @@ export function Avatar() {
   const { firstName, lastName } = authUser;
 
   return (
-    <div className="flex items-center justify-center w-10 h-10 bg-indigo-600 text-white font-bold rounded-full">
-      {firstName[0]}
-      {lastName[0]}
-    </div>
+    <Menu.Wrapper>
+      <Menu.Button>
+        <div className="flex items-center justify-center w-10 h-10 bg-indigo-600 text-white font-bold rounded-full">
+          {firstName[0]}
+          {lastName[0]}
+        </div>
+      </Menu.Button>
+      <Menu.List>
+        <Menu.Item onClick={handleLogout}>
+          <p>Logout</p>
+        </Menu.Item>
+      </Menu.List>
+    </Menu.Wrapper>
   );
 }
