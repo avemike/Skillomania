cbc3a7ce1454a30be0e89435e324920d14dd3271
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-10-07 17:09:47 +0200

    main components frontend

--- client/src/pages/AccountPage.tsx
+++ client/src/pages/AccountPage.tsx
@@ -18,23 +18,43 @@ export function AccountPage() {
       <Flex direction="row">
         <Flex
           direction="column"
-          width="15%"
-          bgColor="gray.100"
+          width="12%"
+          bgColor="gray.900"
           h="calc(100vh)"
-          pl="5"
           pt="5"
           gap="5"
+          boxShadow="md"
         >
-          <Box>
-            <Text>Catalog</Text>
+          <Box pb="10">
+            <Text color="gray.200" ml="4" fontSize="lg" as="b">
+              Skillomania
+            </Text>
+          </Box>
+          <Box borderBottom="1px" borderColor="gray.400" pb="3">
+            <Text color="gray.200" ml="4" fontSize="lg" as="b">
+              Dashboard
+            </Text>
+          </Box>
+          <Box borderBottom="1px" borderColor="gray.400" pb="3">
+            <Text color="gray.200" ml="4" mt="3" mb="3">
+              Catalog
+            </Text>
             <UnorderedList>
-              <ListItem>Quick challenge</ListItem>
-              <ListItem>Challenge paths</ListItem>
-              <ListItem>Lists</ListItem>
+              <ListItem color="gray.200" ml="4" fontSize="sm">
+                Quick challenge
+              </ListItem>
+              <ListItem color="gray.200" ml="4" fontSize="sm">
+                Challenge paths
+              </ListItem>
+              <ListItem color="gray.200" ml="4" fontSize="sm">
+                Lists
+              </ListItem>
             </UnorderedList>
           </Box>
-          <Box>
-            <Text>My challenges</Text>
+          <Box borderBottom="1px" borderColor="gray.400" pb="3">
+            <Text color="gray.200" ml="4">
+              My challenges
+            </Text>
             <Box
               height={10}
               width={10}
@@ -44,26 +64,90 @@ export function AccountPage() {
               margin="2"
               alignContent="center"
               textAlign="center"
+              ml="25%"
+              my="4"
+              color="gray.900"
             >
               3
             </Box>
           </Box>
           <Box>
             {" "}
-            <Text>Challenge manager</Text>
+            <Text color="gray.200" ml="4">
+              Challenge manager
+            </Text>
           </Box>
         </Flex>
-        <Flex
-          direction="row"
-          gap="2"
-          pt="2"
-          pr="5"
-          justifyContent="end"
-          width="100%"
-        >
-          <Box h="10" w="10" bgColor="pink" borderRadius="full"></Box>
-          <Box h="10" w="10" bgColor="pink" borderRadius="full"></Box>
-          <Avatar name="h g" h="10" w="10"></Avatar>
+        <Flex direction="column" width="100%" height="100%">
+          <Flex
+            direction="row"
+            gap="2"
+            pt="2"
+            pr="5"
+            justifyContent="end"
+            width="100%"
+            height="20%"
+          >
+            <Box h="10" w="10" bgColor="gray.600" borderRadius="full"></Box>
+            <Box h="10" w="10" bgColor="gray.600" borderRadius="full"></Box>
+            <Avatar name="h g" h="10" w="10"></Avatar>
+          </Flex>
+
+          <Flex
+            height="calc(90vh)"
+            width="100%"
+            direction="row"
+            ml="24%"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Box bgColor="gray.900" width="30%" height="70%">
+              <Text
+                borderBottom="1px"
+                borderColor="gray.400"
+                py="3"
+                mx="10"
+                color="white"
+                fontSize="lg"
+              >
+                My challenges
+              </Text>
+            </Box>
+            <Flex
+              direction="column"
+              height="100%"
+              width="100%"
+              alignItems="start"
+              justifyContent="center"
+              gap="5%"
+              ml="5%"
+            >
+              <Box bgColor="gray.900" width="30%" height="30%">
+                <Text
+                  borderBottom="1px"
+                  borderColor="gray.400"
+                  py="3"
+                  mx="10"
+                  color="white"
+                  fontSize="lg"
+                >
+                  Tasks completed
+                </Text>
+              </Box>
+              <Box bgColor="gray.900" width="30%" height="30%">
+                <Text
+                  borderBottom="1px"
+                  borderColor="gray.400"
+                  py="3"
+                  mx="10"
+                  color="white"
+                  fontSize="lg"
+                >
+                  Recommended for you
+                </Text>
+              </Box>
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </Box>
