ff0a52f08005f74d30386df10c810c98b97d04b5
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-10-30 21:14:15 +0100

    add transition skeleton slide from the top

--- client/src/pages/Randomizer.tsx
+++ client/src/pages/Randomizer.tsx
@@ -1,47 +1,58 @@
-// import { Box, Button, Center, Flex, Slide, Lorem } from "@chakra-ui/react";
-// import { useState } from "react";
-// /**
-//  * Renders the Home component.
-//  * @remarks
-//  * WIP: Currently function as a placeholder for the homepage.
-//  */
-// export function Random() {
-//     setIsPanelHidden((previousPanelHidden) => !previousPanelHidden);
-//     wez to ogarnij z welcomepage jak to zaimplementowac
-//   return (
-//     <>
-//       {" "}
-//       <Flex
-//         w="calc(100vw)"
-//         h="calc(100vh)"
-//         alignContent="center"
-//         justifyContent="center"
-//       >
-//         <Center>
-//           <Box
-//             w="100%"
-//             p={4}
-//             maxW="sm"
-//             borderRadius="lg"
-//             overflow="hidden"
-//             textAlign="center"
-//           >
-//             <Button onClick={onToggle}>Surprise me!</Button>
-//           </Box>
-//           <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
-//             <Box
-//               p="40px"
-//               color="white"
-//               mt="4"
-//               bg="teal.500"
-//               rounded="md"
-//               shadow="md"
-//             >
-//               <Lorem count={2} />
-//             </Box>
-//           </Slide>
-//         </Center>
-//       </Flex>
-//     </>
-//   );
-// }
+import { Box, Button, Center, Flex, Slide } from "@chakra-ui/react";
+import { useState } from "react";
+/**
+ * Renders the Home component.
+ * @remarks
+ * WIP: Currently function as a placeholder for the homepage.
+ */
+export function Random() {
+  const [isPanelHidden, setIsPanelHidden] = useState<boolean>(true);
+
+  const toggle = () => {
+    setIsPanelHidden((previousPanelHidden) => !previousPanelHidden);
+  };
+  return (
+    <>
+      {" "}
+      <Flex
+        w="calc(100vw)"
+        h="calc(100vh)"
+        alignContent="center"
+        justifyContent="center"
+      >
+        <Center>
+          <Box
+            w="100%"
+            p={4}
+            maxW="sm"
+            borderRadius="lg"
+            overflow="hidden"
+            textAlign="center"
+          >
+            <Button onClick={toggle}>Surprise me!</Button>
+          </Box>
+        </Center>
+      </Flex>
+      {!isPanelHidden ? (
+        <Box
+          height="100%"
+          width="100%"
+          zIndex="10"
+          position="absolute"
+          top="0"
+          left="0"
+          backdropFilter="auto"
+          backdropBlur="8px"
+          backdropContrast="80%"
+          onClick={toggle}
+        >
+          <Slide in={!isPanelHidden} direction="top" style={{ zIndex: 10 }}>
+            <Box height="50vh" width="40vw" bgColor="blue"></Box>
+          </Slide>
+        </Box>
+      ) : (
+        <Box></Box>
+      )}
+    </>
+  );
+}
